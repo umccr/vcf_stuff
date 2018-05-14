0fec8c93a94ffa01602a422359d5234ce54bc0b3
vladsaveliev <vladislav.sav@gmail.com>
2018-05-14 14:49:24 +1000

    Minor fix

--- vcf_stuff/panel_of_normals/annotate.py
+++ vcf_stuff/panel_of_normals/annotate.py
@@ -36,7 +36,7 @@ def main(vcf, genome, output_file=None, filter_hits=None):
     normals_dir = get_ref_file(genome, key='panel_of_normals_dir')
     run_simple(f'sed s#file=\\\"#file=\\\"{normals_dir}/# {get_indels_toml_path()} > {fixed_indels_toml_f.name}')
 
-    run_simple(f'cat {fixed_indels_toml_f.name)}')
+    run_simple(f'cat {fixed_indels_toml_f.name}')
 
     cmd = (f'vcfanno {fixed_snps_toml_f.name} {vcf}' +
         f' | vcfanno -permissive-overlap {fixed_indels_toml_f.name} /dev/stdin | bgzip -c')
