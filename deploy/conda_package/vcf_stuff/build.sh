886ee32e7a971cf188989e1a7c0768afa067608c
vladsaveliev <vladislav.sav@gmail.com>
2018-11-02 10:24:52 +1100

    Conda build.sh: remove pkg name

--- deploy/conda_package/vcf_stuff/build.sh
+++ deploy/conda_package/vcf_stuff/build.sh
@@ -1,4 +1,4 @@
 # `--single-version-externally-managed --root=/` is added to make setuptools avoid downloading dependencies. This resolves the conda-build
 #       error "Setuptools downloading is disabled in conda build. Be sure to add all dependencies in the meta.yaml"
 $PYTHON setup.py install --single-version-externally-managed --root=/
-chmod -R o+r $PREFIX/lib/python*/site-packages/hpc_utils*
+chmod -R o+r $PREFIX/lib/python*/site-packages/*
