c6587d6d9664b4327d474c7b9576bd884b626b00
vladsaveliev <vladislav.sav@gmail.com>
2019-01-17 23:30:17 +1100

    Add script to generate coding bed for hp calling

--- vcf_stuff/filtering/hmf/generate_coding_bed.py
+++ vcf_stuff/filtering/hmf/generate_coding_bed.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+from bed_annotation import get_canonical_transcripts_ids
 from hpc_utils.hpc import get_ref_file
 # from pybedtools import BedTool
 from ngs_utils import gtf
@@ -9,15 +9,27 @@ from ngs_utils import gtf
 '''
 
 GENOME = 'GRCh37'
-OUTPUT= 'coding_regions.bed'
+OUTPUT= 'coding_regions.canonical.bed'
 
 gtf_fpath = get_ref_file(GENOME, key='gtf')
 db = gtf.get_gtf_db(gtf_fpath)
 
+canon_by_gname = get_canonical_transcripts_ids(GENOME)
+# def get_only_canonical_filter():
+#     return lambda x: x[BedCols.ENSEMBL_ID] == canon_tx_by_gname.get(x[BedCols.HUGO]) or \
+#                      x[BedCols.ENSEMBL_ID] == canon_tx_by_gname.get(x[BedCols.GENE])
+
+def _get_attr(_rec, _key):
+    val = _rec.attributes.get(_key)
+    if val is None:
+        return None
+    assert len(val) == 1, (_key, str(val))
+    return val[0]
+
 features = []
 for rec in db.all_features(order_by=('seqid', 'start', 'end')):
     if rec.end - rec.start < 0: continue
-    if rec.featuretype == 'CDS':
+    if rec.featuretype == 'CDS' and _get_attr(rec, 'transcript_id') in canon_by_gname.values():
         features.append([rec.chrom,
                          str(rec.start - 1),
                          str(rec.end)])
@@ -26,3 +38,7 @@ with open(OUTPUT, 'w') as out:
     for f in features:
         out.write('\t'.join(f) + '\n')
 #BedTool(features).saveas(OUTPUT)
+
+
+# Then sort and clean:
+# sort -k1,1V -k2,2n coding_regions.canonical.bed | grep -v ^MT | grep -v ^GL > coding_regions.sort.bed
