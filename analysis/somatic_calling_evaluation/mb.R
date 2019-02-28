e6b203f9fc1a07de6e332854a69caee169208413
vladsaveliev <vladislav.sav@gmail.com>
2019-02-28 18:38:33 +1100

    Update R somatic filtering exploration

--- analysis/somatic_calling_evaluation/mb.R
+++ analysis/somatic_calling_evaluation/mb.R
@@ -1,4 +1,3 @@
-library(ggplot2)
 library(stringr)
 library(stringi)
 library(plyranges)
@@ -8,14 +7,22 @@ library(crayon)
 library(bedr)
 # devtools::install_github("r-lib/pillar")
 # library(pillar)
-library(plyr)
-library(tidyr)
 library(patchwork)
+if("dplyr" %in% (.packages())){
+  detach("package:ggplot2", unload=TRUE) 
+  detach("package:dplyr", unload=TRUE) 
+  detach("package:plyr", unload=TRUE) 
+} 
+library(plyr)
+library(ggplot2)
 library(dplyr)
 
-dir = "/Users/vsaveliev/Analysis/snv_validation/mb/"
-truth_vcf = read.vcf(str_c(dir, "MB-benchmark.ANNO.FILT.vcf.gz"), split.info = T)
-called_vcf = read.vcf(str_c(dir, "batch1-ensemble-annotated.ANNO.FILT.TP.vcf.gz"), split.info = T)
+dir = "/Users/vsaveliev/Analysis/snv_validation/mb/ICGC_MB/"
+truth_vcf = read.vcf(str_c(dir, "MB-benchmark.ANNO.FILT.vcf.gz"),
+                     split.info = T, split.samples = T)
+called_vcf = read.vcf(str_c(dir, "batch1-ensemble-annotated.ANNO.FILT.TP.SAMPLE.vcf.gz"), 
+                      split.info = T)
+called_vcf$vcf$NM = str_split(called_vcf$vcf$tumor_downsample, ":", simplify = T)[, 10] %>% as.double()
 
 # proc_tricky = function(vcf) {
 #   for (key in names(vcf$vcf)) {
@@ -48,18 +55,17 @@ count_status <- function(.data) {
 
 fix_fields <- function(data) {
   renamed <- data %>% 
-    dplyr::mutate(
+    mutate(
       HMF_MAPPABILITY = map_dbl(map(str_split(HMF_MAPPABILITY, ","), as.double), min)
     ) %>% 
-    dplyr::select(
+    select(
       # -AF,
       # -VD,
       # -DP,
-      -GIAB_CONF,
       -QUAL,
       -ID
     ) %>% 
-    dplyr::rename(
+    rename(
       GENE = PCGR_SYMBOL,
       TCGA = PCGR_TCGA_PANCANCER_COUNT,
       ICGC = ICGC_PCAWG_HITS,
@@ -84,12 +90,12 @@ merged <- full_join(
     called_vcf$vcf %>% as_tibble() %>% fix_fields() %>% select(-TIERS),
     by = c('CHROM', 'POS', 'REF', 'ALT'),
     suffix = c('.truth', '.called')) %>% 
-  dplyr::select(
+  select(
     -AF,
     -VD,
     -DP
   ) %>% 
-  dplyr::rename(
+  rename(
     AF = TUMOR_AF.called,
     VD = TUMOR_VD.called,
     DP = TUMOR_DP.called,
@@ -97,17 +103,18 @@ merged <- full_join(
     # 'COSM', 'ENCODE', 'GIAB', 'MBL', 'HMF_HS', 'ICGC', 'PCGR_TIER', 'CLNSIG', 
     # 'CSQ', 'DRIVER', 'PCGR_HS', 'TCGA', 'GENE', 'TRICKY', 'PoN'
   ) %>% 
-  dplyr::mutate(
+  mutate(
     vartype = get_type(REF, ALT),
     is_snp = vartype == "SNP",
     is_called = !is.na(FILT),
-    is_passed = is_called & FILT == 'PASS',
+    umccrise_passed = is_called & FILT == 'PASS',
+    is_passed = umccrise_passed,
     is_true = !is.na(TIERS),
     AF = as.double(AF),
     DP = as.integer(DP),
     VD = round(AF * DP)
   ) %>% 
-  dplyr::mutate(
+  mutate(
     GENE = nonna(GENE.called, GENE.truth),
     TCGA = nonna(TCGA.called, TCGA.truth),
     ICGC = nonna(ICGC.called, ICGC.truth),
@@ -129,9 +136,9 @@ merged <- full_join(
   ) %>% 
   count_status()
 
-# TODO: why CLNSIG is missing??? Check PCGR run
 merged %>% count(HS)
-merged %>% count(COSM)
+merged %>% count(MQ)
+merged %>% count(NM)
 
 f_measure <- function(b, prec, recall) {
   Fmeasure <- (1 + b**2) * prec * recall / (b**2 * prec + recall)
@@ -157,7 +164,6 @@ build_stats <- function(.data) {
   metrics = names(stats %>% select(-vartype))
   
   # If you want samples to be columns, and metrics to be rows:
-  print(stats)
   stats_row <- stats %>%
     gather(metric, s, -vartype) %>%
     mutate(metric = factor(metric, levels = metrics)) %>%
@@ -190,23 +196,19 @@ build_stats <- function(.data) {
 
 show_stats = function(...) {
   stat_dfs = map(list(...), build_stats)
-  res <- join_all(stat_dfs, by = c("metric", "vartype"), match = "all")  # suffix = argnames)
+  res = join_all(stat_dfs, by = c("metric", "vartype"), match = "all")  # suffix = argnames)
   argnames = sapply(substitute(list(...))[-1], deparse)  # %>% str_c(".", .)
-  names(res)[c(-1, -2)]  <- argnames
+  names(res)[c(-1, -2)] = argnames
   res
 }
 
 ##############
 ### Exploring
 called <- merged %>% mutate(is_passed = is_called)
-passed <- merged
+passed <- merged %>% mutate(is_passed = umccrise_passed)
 show_stats(called, passed)
 
-# brad_filt <- merged %>% mutate(is_passed = 
-# QD < 10.0 && AD[1] / (AD[1] + AD[0]) < 0.25 && ReadPosRankSum < 0.0
-# merged %>% filter(!is.na(ReadPosRankSum)) %>% count(CALLERS)
-
-plot_freq <- function(.data, metric, binwidth = NULL) {
+plot_freq <- function(.data, metric) {
   .data %>% 
     count_status() %>% 
     filter(!is_tn) %>% 
@@ -215,35 +217,86 @@ plot_freq <- function(.data, metric, binwidth = NULL) {
     ggplot() +
     # geom_histogram(aes(get(metric), colour = status), binwidth = 0.05)
     # geom_density(aes(get(metric), colour = status))
-    geom_freqpoly(aes(.data[[metric]], colour = status), binwidth = binwidth) +
+    geom_density(aes(.data[[metric]], colour = status)) +
     # geom_freqpoly(aes(get(metric), colour = status), bins = 30) +
     facet_wrap(~vartype, nrow = 2, scales = "free_y")
 }
-
-plot_freq(merged, "AF") + plot_freq(merged, "VD")
+plot_freq(passed, "NM")
+plot_freq(passed, "MQ")
+plot_freq(passed, "AF")
+plot_freq(passed, "VD")
 # 1. False positive SNPs are predominantly on low frequencies. 
 #    False negative SNPs are on every AF, though VD tend to be low.
 # 2. False positive indels are at any freq but mostly only below 0.35.
 #    False negative inels are on 0.4-0.5, with VD at 40x - 
 #    same shape as AF, so does not depend on DP much
 
-plot_freq(merged, "AF", binwidth = 0.05) + plot_freq(merged %>% filter(TUMOR_VD.called < 25), "VD", binwidth = 1)
+#############
+## Definding filtering functions
+reject_if = function(.data, cond) {
+  # Rejects additionally to existing is_passed
+  cond_q <- substitute(cond)
+  .data %>% mutate(is_passed = is_called & ifelse(eval(cond_q, .data) %in% c(F, NA), is_passed, F))
+}
 
-vd8 = merged %>% mutate(is_passed = (TUMOR_VD.called >= 8 | is.na(TUMOR_VD.called)))
-passed_vd8 = merged %>% mutate(is_passed = (is_passed & TUMOR_VD.called >= 8 | is.na(TUMOR_VD.called)))
-show_stats(called, passed, vd8, passed_vd8)
-# apparently VD>=8 is the best filter. TODO: explore which filters we should remove
+keep_if = function(.data, cond) {
+  # Override existing is_passed if cond is true
+  cond_q <- substitute(cond)
+  .data %>% mutate(is_passed = is_called & ifelse(eval(cond_q, .data) %in% c(F, NA), is_passed, T))
+}
 
-plot_freq(vd8, "TUMOR_AF.called", binwidth = 0.05) + 
-  plot_freq(vd8 %>% filter(TUMOR_VD.called < 25), "TUMOR_VD.called", binwidth = 1)
+df = tribble(
+  ~is_called, ~umccrise_passed, ~is_true, ~NM, ~vartype,
+  TRUE,       TRUE,             TRUE,     1,   "snp",
+  TRUE,       F,                TRUE,     1,   "snp",                
+  F,          F,                TRUE,     1,   "snp",
+  TRUE,       TRUE,             F,        1,   "snp",
+  TRUE,       F,                F,        1,   "snp",
+  TRUE,       TRUE,             TRUE,     0,   "snp",
+  TRUE,       F,                TRUE,     0,   "snp",                
+  F,          F,                TRUE,     0,   "snp",
+  TRUE,       TRUE,             F,        0,   "snp",
+  TRUE,       F,                F,        0,   "snp",
+  TRUE,       TRUE,             TRUE,     NA,  "snp",
+  TRUE,       F,                TRUE,     NA,  "snp",                
+  F,          F,                TRUE,     NA,  "snp",
+  TRUE,       TRUE,             F,        NA,  "snp",
+  TRUE,       F,                F,        NA,  "snp"
+) %>% 
+  mutate(is_passed = is_called & umccrise_passed)
 
-plot_freq(passed_vd8, "TUMOR_AF.called", binwidth = 0.05) + 
-  plot_freq(passed_vd8 %>% filter(TUMOR_VD.called < 25), "TUMOR_VD.called", binwidth = 1)
-# VD>=8 good for indels, but not that good for SNPs
+show_stats(df, df %>% reject_if(NM < 1), df %>% keep_if(NM >= 1))
+df
+df %>% reject_if(NM < 1)
+df %>% keep_if(NM >= 1)
+
+###########
+## Exploring filters
+vd8_fi = passed %>% reject_if(VD < 8)
+vd8_ki = passed %>% keep_if(VD >= 8 | is.na(VD))
+allaf = passed %>% keep_if(FILT == "AF10") 
+allaf_vd8_fi = allaf %>% reject_if(VD < 8)
+allaf_vd8_ki = allaf %>% keep_if(VD >= 8 | is.na(VD))
+show_stats(passed, vd8_fi, vd8_ki, allaf, allaf_vd8_fi, allaf_vd8_ki)
+# Apparently keeping all VD>=8 improves the F2 measure. However, we again have trust issues
+#   with the truth set.
+
+# brad_filt = passed %>% filt_var(QD < 10.0 && AD[1] / (AD[1] + AD[0]) < 0.25 && ReadPosRankSum < 0.0
+# bcbio filter:
+bcbio_filt = allaf %>% reject_if(VD < 6 & (MQ < 60.0 & NM > 2.0 | MQ < 55.0 & NM > 1.0))
+show_stats(allaf, allaf_vd8_fi, allaf_vd8_ki, bcbio_filt)
+
+cool_filt = allaf_vd8_ki %>% reject_if(MQ < 60.0 & NM > 2.0 | MQ < 50.0 & NM > 1.0)
+show_stats(allaf, allaf_vd8_fi, allaf_vd8_ki, bcbio_filt, cool_filt)
 
 
 
-### Exploring FN
+
+
+
+
+#################
+## Exploring FN
 passed %>% count(HS)
 
 passed %>% filter(HS) %>% select(CHROM, POS, REF, ALT, AF, VD, FILT, GIAB, PoN, HMF_HS, TRICKY,
